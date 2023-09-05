547b4ede1f8a2c999117e311751d305c28c2f5be
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-05 20:14:54 -0300

    Implemented bulk update controller

---
+++ backend/src/Controllers/BulkUpdateController.ts
@@ -0,0 +1,126 @@
+import { NextFunction, Request, Response } from "express";
+import prisma from "../Services/db";
+
+import { CSVRequestType } from "../middlewares/CSVParserMiddleware";
+import { ThrowInternalServerError } from "../Util/ServerErrorHandler";
+
+class BulkUpdateController {
+    async Validate(req: Request<unknown, unknown, CSVRequestType>, res: Response, next: NextFunction) {
+        const reqItems = req.body;
+        if (!reqItems.items) {
+            return res.status(500).json({ error: "Nenhum item válido" });
+        }
+
+        const validItems = reqItems.items.filter(item => item.code > 0);        
+        const validCodes = validItems.map(item => item.code);        
+        
+        try {
+            // Validar se todos os itens estão presentes no banco de dados
+            const products = await prisma.products.findMany({
+                where: { code: { in: validCodes } },
+                include: { product_idToproducts: true }
+            });
+            const packs = await prisma.packs.findMany({
+                where: { pack_id: { in: validCodes } } ,                    
+            });
+
+            reqItems.items.forEach(item => {
+                if (item.code > 0) {
+                    item.product = products.find(product => Number(product.code) === item.code);
+                    item.packs = packs.filter(pack => Number(pack.pack_id) === item.code);
+
+                    if (!item.product) { item.errors.push(`Produto ${item.code} não cadastrado`); }
+                    if (item.packs.length == 0 && item.product) item.type = "Product";
+                    if (item.packs.length > 0) item.type = "Pack";
+                }
+            });                
+
+            reqItems.items.forEach(item => {
+                // Validações de preço
+                if (item.product && item.new_price < Number(item.product.cost_price)) {
+                    item.errors.push("Preço de venda está menor que o preço de custo");
+                }
+                if (item.product &&
+                    Math.abs(item.new_price - Number(item.product.sales_price)) > 0.1 * Number(item.product.sales_price)) {
+                    item.errors.push("Novo preço de venda é diferente em mais que 10% comparado ao anterior");
+                }
+
+                // Validações de pacotes
+                // Para os produtos que pertencem a pacotes, a atualização de preço do pacote deve estar no CSV
+                if (
+                    item.type === "Product" &&
+                    item.product?.product_idToproducts &&
+                    item.product.product_idToproducts.length > 0
+                ) {
+                    item.product.product_idToproducts.forEach(pack => {
+                        if (!validCodes.includes(Number(pack.pack_id))) {
+                            item.errors.push(`Pacote ${pack.pack_id} não incluido no CSV`);
+                        }
+                    });
+                }
+                // Todos os produtos de pacotes informados devem estar presentes no CSV
+                if (
+                    item.type === "Pack"
+                ) {
+                    let sum = 0;
+                    item.packs?.forEach(product => {
+                        const productInCSV = reqItems.items?.find(p => p.code === Number(product.product_id));                  
+                        if (!productInCSV) {
+                            item.errors.push(`Produto ${product.product_id} não incluido no CSV`);
+                        } else {
+                            sum += productInCSV.new_price * Number(product.qty);
+                        }
+                    });
+
+                    if (sum !== item.new_price) {
+                        item.errors.push("A soma dos preços de venda dos produtos neste pacote é diferente do novo valor do pacote");
+                    }
+                }
+
+                if (item.errors.length > 0) { req.body.badRequest = true; }
+            });            
+
+        } catch (e) {
+            return ThrowInternalServerError(res, e);
+        }        
+        
+        if (req.body.badRequest) {
+            return res.status(400).json(reqItems.items);
+        }
+        else 
+        {
+            if (req.method === "PUT") {            
+                next();
+            }
+            else
+            {
+                return res.json(reqItems.items);
+            }            
+        }
+
+    }
+
+    async Update(req: Request<unknown, unknown, CSVRequestType>, res: Response) {
+        const reqItems = req.body.items;
+
+        const productsToUpdate = reqItems?.map(p => ({
+            where: { code: p.code },
+            data: { sales_price: p.new_price }
+        }));
+
+        if (!productsToUpdate) {
+            return res.status(500).json({ error: "productsToUpdate está indefinido" });
+        }
+
+        try {
+            for (const product of productsToUpdate) {
+                await prisma.products.update(product);
+            }
+            return res.json({ message: `${productsToUpdate.length} products have been updated` });
+        } catch (e) {
+            return ThrowInternalServerError(res, e);
+        }
+    }
+}
+
+export default new BulkUpdateController();
