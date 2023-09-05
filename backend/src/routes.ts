2ecccc65583846ce6f667b230bca4b25ba49463a
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-05 20:15:12 -0300

    Defined routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,19 @@
 import { Router, Response } from "express";
+import ProductController from "./Controllers/ProductController";
+import PackController from "./Controllers/PackController";
+import BulkUpdateController from "./Controllers/BulkUpdateController";
+import { CSVParseToRows, CSVRowsToBulkUpdateItem } from "./middlewares/CSVParserMiddleware";
+
+
 
 const routes = Router();
 
 routes.get("/", (_, res: Response) => { return res.json("Hello!"); });
+
+routes.get("/product/:code", ProductController.Read);
+routes.get("/pack/:id", PackController.Read);
+
+routes.post("/bulkupdate", CSVParseToRows, CSVRowsToBulkUpdateItem, BulkUpdateController.Validate);
+routes.put("/bulkupdate", CSVParseToRows, CSVRowsToBulkUpdateItem, BulkUpdateController.Validate, BulkUpdateController.Update);
 
 export default routes;
