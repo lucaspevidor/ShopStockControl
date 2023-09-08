fd3c7dcdb919f26ef062130bd523e75c4f7174d6
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-08 10:18:18 -0300

    Updated API service to test for VITE_API_URL env variable

--- frontend/src/Services/api.ts
+++ frontend/src/Services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
     headers: {
         "Content-Type": "application/json"
     }
